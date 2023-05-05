fe41bdf9a8c5bfc50ce1001de0950e3f556fc730
jaraujo09 <120012238+jaraujo09@users.noreply.github.com>
2023-05-05 17:18:36 +0100

    update aula 4 - automata

--- Aula 3 - Procura de Padrões em Sequências/Automata.py
+++ Aula 3 - Procura de Padrões em Sequências/Automata.py
@@ -3,7 +3,12 @@
 
 class Automata:
     
-    def __init__(self, alphabet, pattern):
+    def __init__(self, alphabet:list[str], pattern:str)->None:
+        """
+        @brief Construtor da class Automata
+        @param alphabet: alfabeto
+        @param pattern: padrão da sequência
+        """
         self.numstates = len(pattern) + 1
         self.alphabet = alphabet
         self.pattern = pattern
@@ -14,18 +19,22 @@ class Automata:
         '''
         Cria a tabela das transições, em que as chaves são tuplos e os valores representam o next state
         '''
-
-        def overlap(s1, s2):
-            maxov = min(len(s1), len(s2))
+        def overlap(s1:str, s2:str)->None:
+            """
+            @brief Função usada para verificar se duas sequências têm um prefixo comum
+            @param s1: sequência 1
+            @param s2: sequência 2
+            """
+            maxov = min(len(s1), len(s2))  #comprimento das duas sequências de entrada
             for i in range(maxov, 0, -1):
-                if s1[-i:] == s2[:i]: return i
-            return 0
+                if s1[-i:] == s2[:i]: return i  #compara os últimos "i" caracteres de s1 com os primeiros "i" caracteres de s2, Se eles forem iguais, a função retorna o valor de "i", que é o comprimento do maior prefixo comum entre as duas sequências
+            return 0 #se nenhum prefixo comum for encontrado, a função retorna 0
 
-        for q in range(self.numstates):
-            for a in self.alphabet:
-                pref = self.pattern[0:q] + a 
-                hit = overlap(pref,self.pattern)
-                self.transitionTable[(q,a)] = hit
+        for q in range(self.numstates): #por cada caractere no padrão
+            for a in self.alphabet:   #por cada simbolo do alfabeto
+                pref = self.pattern[0:q] + a #padrão que corresponde a esse estado e símbolo de entrada
+                hit = overlap(pref,self.pattern) #encontrar o comprimento do maior prefixo comum entre essa string "pref" e o padrão completo que o autómato procura
+                self.transitionTable[(q,a)] = hit #valor retornado pela função overlap é armazenado na tabela de transição na posição correspondente ao estado e símbolo de entrada
        
     def printAutomata(self):
         print ("States: " , self.numstates)
@@ -36,46 +45,39 @@ class Automata:
          
     def nextState(self, current:int, symbol:str)->int:
         """
-        Vai à tabela de transições e, de acordo com o símbolo atual, altera o seu estado
-        Args:
-            current: estado atual
-            symbol: nucleótido a analisar
-        Returns:
-            valor inteiro representativo do estado
+        @brief Vai à tabela de transições e, de acordo com o símbolo atual, altera o seu estado
+        @param current: estado atual
+        @param symbol: nucleótido a analisar
+        @returns valor inteiro representativo do estado
         """
         return self.transitionTable.get((current, symbol)) #current - estado atual; symbol - simbolo de sequencia 
-    
     #Poe se 2 parenteses porque a nossa key é o tuplo estado corrente e o symbolo que queremos analisar
         
     def applySeq(self, seq:str)->list[int]:
         """
-        Processa a sequência e devolve uma lista dos estados, iniciando no estado zero
-        Args:
-            seq: sequência a processar
-        Returns:
-            lista de inteiros com os estados da sequência
+        @brief Processa a sequência e devolve uma lista dos estados, iniciando no estado zero
+        @param seq: sequência a processar
+        @returns  devolve uma lista de inteiros que representa os estados visitados pelo autómato durante o processamento
         """
         q = 0
         res = [q]
         for n in seq:
-            q = self.nextState(q, n)
-            res.append(q)
+            q = self.nextState(q, n) #o próximo estado é armazenado na variável q
+            res.append(q) #adiciona-se à lista res que contém todos os estados visitados até o momento
         return res
         
     def occurencesPattern(self, text:str)->list[int]: # posiçoes onde iniciam os p
         """
-        Função que devolve a lista de indices das posições iniciais dos padrões
-        Args:
-            text: sequência a analisar
-        Returns:
-            lista de índices das posições iniciais de onde se encontra o padrão
+        @brief Função que devolve a lista de indices das posições iniciais dos padrões
+        @param text: sequência a analisar
+        @returns lista de índices das posições iniciais de onde se encontra o padrão
         """
         q = 0 
         res = []
         for n in range(len(text)):
-            q = self.nextState(q, text[n])
-            if q == self.numstates-1: 
-                res.append(n-self.numstates+2)
+            q = self.nextState(q, text[n]) #obtem o próximo estado do autómato, dado o estado atual e o símbolo do alfabeto de entrada
+            if q == self.numstates-1: #se o próximo estado for o estado final do autómato, significa que o padrão foi encontrado na posição atual da sequência
+                res.append(n-self.numstates+2) #adiciona a posição inicial do padrão (calculada como n-self.numstates+2) à lista res que contém todas as posições iniciais onde o padrão é encontrado
         return res
 
 

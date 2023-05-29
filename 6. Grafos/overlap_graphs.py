d22a695eb18fa1e67fcd49312092d12f8ce0a987
jaraujo09 <120012238+jaraujo09@users.noreply.github.com>
2023-05-29 15:50:05 +0100

    update overlap_graph

--- 6. Grafos/overlap_graphs.py
+++ 6. Grafos/overlap_graphs.py
@@ -15,7 +15,11 @@ class OverlapGraph(MyGraph):
         
     
     ## create overlap graph from list of sequences (fragments)
-    def create_overlap_graph(self, frags):
+    def create_overlap_graph(self, frags:list[str])->None:
+        """
+        @brief Cria um grago de sobreposição com base numa lista de fragmentos
+        @param frags:lista de fragmentos da sequência
+        """
         for x in frags: #por sequencia em frag
             self.add_vertex(x)  #cria vértice no grafo
         for seq in frags:
@@ -25,7 +29,11 @@ class OverlapGraph(MyGraph):
                         self.add_edge(seq,seq2) #adiciona um edge que é um tuplo  (sufixo,prefixo)
 
         
-    def create_overlap_graph_with_reps(self, frags):  # caso de replicas de fragmentos
+    def create_overlap_graph_with_reps(self, frags:list[str])->None:  # caso de replicas de fragmentos
+        """
+        @brief Cria um grafo de sobreposição com base em uma lista de fragmentos, tendo em consideração fragmentos replicados.
+        @param frags: lista de fragmentos da sequência
+        """
         idnum = 1
         for seq in frags:
             self.add_vertex(seq + '-' + str(idnum)) #vai adicionar o vértice com a sequencia + um identificador associado que começa em 1
@@ -40,32 +48,67 @@ class OverlapGraph(MyGraph):
                         #o de destino que vai ser o y que  cada elemento vindo da lista da função get_instances
             idnum += 1
     
-    def get_instances(self, seq):
+    def get_instances(self, seq:str)->list[str]:
+        """
+        @brief Retorna uma lista contendo todas as instâncias de uma sequência no grafo
+        @param seq: sequência
+        @return lista de instância da sequência
+        """
         res = []
         for k in self.graph.keys():
             if seq in k: res.append(k)
         return res
     
-    def get_seq(self, node):
+    def get_seq(self, node:str)->str or None:
+        """
+        @brief Retorna a sequência associada a um nó do grafo
+        @param node: nó do grafo
+        @return A sequência associada ao nó ou None se o nó não existir no grafo
+        """
         if node not in self.graph.keys(): return None
         if self.reps: return node.split("-")[0]
         else: return node
     
-    def seq_from_path(self, path):
-        # ...
+    def seq_from_path(self, path:list[str])->str or None:
+        """
+        @brief Gera uma sequência a partir de um caminho no grafo de sobreposição, se o caminho for um caminho hamiltoniano válido.
+        @param path caminho no grafo de sobreposição.
+        @return A sequência gerada a partir do caminho ou None se o caminho não for válido
+        """
+        if not self.check_if_hamiltonian_path(path):  #vê se o caminho é hamiltoniano
+            return None #se não for dá return None
+        seq = self.get_seq(path[0]) #as 3 primeiras letras da seq vão ser as 3 da primeira read do path
+        for i in range(1, len(path)):  #por cada i no tamanho do path, iniciado em 1
+            prox = self.get_seq(path[i]) #encontra-se a proxima seq no path e vamos buscar apenas a seq, sem o id
+            seq += prox[-1] #adiciona à seq apenas a ultima letra, pois as anteriores são prefixos da seq
         return seq    
    
                     
 # auxiliary
-def composition(k, seq):
+def composition(k:int, seq:str)->list:
+    """
+    @brief Função que segmenta em conjuntos de tamanho k
+    @param k: kmers
+    @para seq: sequência a segmentar
+    @return lista de kmers de forma ordenada"""
     res = []
-    #...
+    for x in range(len(seq)-k+1): #por cada x no range do tamanho da seq - k+1
+        res.append(seq[x:x+k])
+    res.sort()
     return res
     
-def suffix (seq): 
+def suffix (seq:str)->str:
+    """
+    @brief Função de sufixo, que retorna toda a sequência exceto o primeiro valor
+    @return string da sequência exceto o primeiro caractere
+    """ 
     return seq[1:]
     
-def prefix(seq):
+def prefix(seq:str)->str:
+    """
+    @brief Função de prefixo, que retorna toda a sequência exceto o último valor
+    @return string da sequência exceto o último caractere
+    """
     return seq[:-1]
 
   
